190a7ec3850bdfd546d71dde1ac3795ad82981fe
hristoterezov <hristo@jitsi.org>
2017-07-09 15:48:55 -0500

    fix(remotecontrol): Error when the sourceId is 0 on windows

--- node_addons/sourceId2Coordinates/index.js
+++ node_addons/sourceId2Coordinates/index.js
@@ -1,5 +1,5 @@
 const sourceId2Coordinates
-    = require('../../build/Release/sourceId2Coordinates.node')
+    = require("../../build/Release/sourceId2Coordinates.node")
         .sourceId2Coordinates;
 
 /**
@@ -11,12 +11,15 @@ const sourceId2Coordinates
  * top left corner of the desktop. Currently works only for windows. Returns
  * undefined for Mac OS, Linux.
  */
-module.exports = function(sourceID) {
-    // On windows the source id will have the following format "0:desktop_id".
+module.exports = function(sourceId) {
+    if(typeof sourceId !== "string" || sourceId === '') {
+        return undefined;
+    }
+    // On windows the source id will have the following format "desktop_id:0".
     // we need the "desktop_id" only to get the coordinates.
-    const idArr = sourceID.split(":");
-    const id = Number(idArr.length > 1 ? idArr[1] : sourceID);
-    if(id) {
+    const idArr = sourceId.split(":");
+    const id = Number(idArr.length > 1 ? idArr[0] : sourceId);
+    if(!isNaN(id)) {
         return sourceId2Coordinates(id);
     }
     return undefined;
