6ae155e884b00286084b02827d4f59d6aa07f135
Leonard Kim <leonard.h.kim@gmail.com>
2018-06-09 11:46:01 -0700

    squash: add comments to constants

--- googleapi/constants.js
+++ googleapi/constants.js
@@ -1,4 +1,12 @@
 module.exports = {
+    /**
+     * The event for when the Google authentication has completed, either
+     * successfully or unsuccessfully.
+     */
     AUTH_FINISHED: 'jitsi-google-auth-finish',
+
+    /**
+     * The event for requesting the Google authentication popup to display.
+     */
     AUTH_REQUESTED: 'jitsi-google-auth-open'
 };
