1369cb54c54a47d0ed2da99c06f9f9c0f3688672
Christoph Settgast <csett86_git@quicksands.de>
2024-04-21 22:26:13 +0200

    ci: update to v4 actions (#387)
    * ci: update to v4 actions
    
    the upload action no longer allows "appending" to the same name, so merge on download as described https://github.com/actions/download-artifact?tab=readme-ov-file#download-multiple-filtered-artifacts-to-the-same-directory
    
    * ci: workaround windows spawns issues
    
    See https://github.com/prebuild/node-gyp-build/issues/68 and https://nodejs.org/en/blog/vulnerability/april-2024-security-releases-2

--- install.js
+++ install.js
@@ -4,6 +4,7 @@ const process = require('process');
 if (process.platform === 'win32') {
 	spawnSync('npm.cmd', ['run', 'node-gyp-build'], {
 		input: 'win32 detected. Ensure native code prebuild or rebuild it',
+		shell: true,
 		stdio: 'inherit'
 	});
 }
